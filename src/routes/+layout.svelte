9fd4c07d5fa68f812eb92156a9110e9576872d93
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2023-01-13 16:44:37 -0800

    fixes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,26 +17,8 @@
 <Analytics />
 
 <style global>
-	a {
-		cursor: url('$lib/images/pointer.png'), auto;
-	}
-
-	button {
-		cursor: url('$lib/images/pointer.png'), auto;
-	}
-
-	html {
-		cursor: url('$lib/images/cursor.png'), auto;
-	}
-
 	.page-wrapper {
 		margin: 0 auto;
 		max-width: 1200px;
-	}
-	/* span {
-	} */
-
-	footer {
-		margin: 0;
 	}
 </style>
