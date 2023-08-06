10840e47d264e5f1cb184e48635401696a4bf937
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2023-08-06 03:15:09 -0700

    API

--- src/routes/hacktoberfest_jam/+page.server.ts
+++ src/routes/hacktoberfest_jam/+page.server.ts
@@ -4,7 +4,7 @@ import { SECRET_GOOGLE_API_KEY } from '$env/static/private';
 export const load = (async () => {
     return {
         props: {
-            googleApiKey: PUBLIC_GOOGLE_API_KEY,
+            googleApiKey: SECRET_GOOGLE_API_KEY,
         },
     };
 }) satisfies PageServerLoad;
